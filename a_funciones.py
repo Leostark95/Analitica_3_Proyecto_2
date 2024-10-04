9fa5c957bb606df82a2e5c850b792e27b740ccc6
Mauela Blandon <manuela.blandon@udea.edu.co>
2024-10-04 11:30:42 -0500

    Inicio mdelos2

--- a_funciones.py
+++ a_funciones.py
@@ -3,3 +3,9 @@ def ejecutar_sql (nombre_archivo, cur):
     sql_as_string=sql_file.read()
     sql_file.close
     cur.executescript(sql_as_string)
+
+def year(db):
+    db['year'] = db['title'].str.extract(r'\((\d{4})\)').astype(int)
+    db['title'] = db['title'].str.replace(r'\(\d{4}\)', '', regex=True).str.strip()
+    
+    return db 
